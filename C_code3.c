448c4a6faf7d63a7562fd3378c7c61f3d601d76f
angry-hemisphere <111339257+angry-hemisphere@users.noreply.github.com>
2023-01-20 02:37:26 +0600

    Update C_code3.c

--- C_code3.c
+++ C_code3.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+void main()
+{
+    int n,i,j;
+    printf("enter the no. of rows for the pattern\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) 
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
