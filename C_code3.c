e5e67dccb8323478912424f688e1b943599aa6fa
angry-hemisphere <111339257+angry-hemisphere@users.noreply.github.com>
2023-01-27 03:01:57 +0600

    Update C_code3.c

--- C_code3.c
+++ C_code3.c
@@ -2,9 +2,9 @@
 void main()
 {
     int n,i,j;
-    printf("enter the no. of rows for the pattern\n");
+    printf("enter the no. of rows for the pattern\n");  //user input for rows of pattern
     scanf("%d",&n);
-    for(i=1;i<=n;i++) 
+    for(i=1;i<=n;i++)                                  // loop for printing the pattern
     {
         for(j=1;j<=i;j++)
         {
