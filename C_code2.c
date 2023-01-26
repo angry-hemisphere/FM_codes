d277d2062678879fe7959c4c364e54b0954a624b
angry-hemisphere <111339257+angry-hemisphere@users.noreply.github.com>
2023-01-27 03:00:00 +0600

    Update C_code2.c

--- C_code2.c
+++ C_code2.c
@@ -1,20 +1,3 @@
-/*#include<stdio.h>
-void main()
-{
-    int integer;
-    char string[100];
-    char character;
-    double decimal;
-    printf("input an integer:");
-    scanf("%d",&integer);
-    printf("input a string:");
-    fgets(string,100,stdin);
-    printf("input a character:");
-    scanf("%c",&character);
-    printf("input a decimal:");
-    scanf("%lf",&decimal);
-    printf("the entered \ninteger:%d \nstring:%s \ncharacter:%c \ndecimal:%lf",integer,string,character,decimal);
-}*/
 #include<stdio.h>
 void main()
 {
