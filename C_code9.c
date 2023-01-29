0759d0bd31201f49fa05e3d0ccaf16dd47923ed2
angry-hemisphere <111339257+angry-hemisphere@users.noreply.github.com>
2023-01-29 17:01:20 +0600

    Update C_code9.c

--- C_code9.c
+++ C_code9.c
@@ -2,13 +2,14 @@
 
 void main() {
     int size;
-    int array[size];
+   
     printf("Enter the size of the array: ");
     scanf("%d", &size);
+     int array[size];
     printf("Enter the elements of the array: \n");
     for(int i = 0; i < size; i++) 
     {
         scanf("%d", &array[i]);
     }
-    printf("The size of the array is: %d\n", sizeof(array);
+    printf("The size of the array is: %d\n", sizeof(array)/sizeof(array[0]));
 }
